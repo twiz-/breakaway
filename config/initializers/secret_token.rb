2f69853fba159d55718aeedd2de89fc45faa8cc1
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-01-11 14:32:58 -0800

    breakway v2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Breakaway::Application.config.secret_key_base = '0b9888aa0cbe78c0ede34c6271add60d5b7bbb69be6c6c0726f51d3c5dd19566dec934d397e7aa8aa57115a4db08e575ace828ba038239eaa1d7a21da9331376'
