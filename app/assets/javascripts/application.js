3be9e7b8429e1472a655422442e83f36d124ee87
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-01-19 11:06:41 -0800

    fix well, add fancybox to manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require thing-fany
+//= require jquery.fancybox
+//= require jquery.fancybox-media
+//= require jquery.fancybox-buttons
+//= require jquery.fancybox-thumbs
+
 //= require_tree .
