46401cbc008780f27d84e6ddc4b35706d3435554
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-01-18 17:43:03 -0800

    add validations to Player model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
     if current_coach
-      # @profiles = current_coach.friends
+       @profiles = current_coach.friends
     else
       @listings = current_player.listings
       @games = current_player.games
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,10 @@
 class Player < User
+  validates :first_name, :last_name, :profile_name, :grad_year, :position, :club_team, :formation, presence: :true
+  validates :profile_name, uniqueness: :true
+  validates :profile_name, format: {
+        with: /\A[a-zA-Z0-9_-]+\z/,
+        message: "must be formatted correctly."
+      }
   
   has_many :listings
   has_many :games
