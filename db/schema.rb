160e299464a33bfecd431ea8f4c3a2d406fe3fd3
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-07-26 19:56:21 -0700

    have users confirm account before signing in for the first time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140118232713) do
+ActiveRecord::Schema.define(version: 20140727022902) do
 
   create_table "coaches", force: true do |t|
     t.datetime "created_at"
@@ -70,8 +70,12 @@ ActiveRecord::Schema.define(version: 20140118232713) do
     t.string   "formation"
     t.string   "position"
     t.string   "profile_name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
