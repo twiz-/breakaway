f103991587c893d06ddb075a4d3a02bf2a1ae2ce
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-07-26 17:04:11 -0700

    remove all html_safe instances, remove extra dash thumb embed from helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,15 +32,6 @@ module ApplicationHelper
       youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
       youtube_id = $5
     end
-    %Q{<img class="thumb-town"src="http://i1.ytimg.com/vi/#{ youtube_id }/mqdefault.jpg">}
-  end
-  def youtube_thumb_embed_dash(youtube_url)
-    if youtube_url[/youtu\.be\/([^\?]*)/]
-      youtube_id = $1
-    else
-      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
-      youtube_id = $5
-    end
-    %Q{<img id="dash-thumb" src="http://i1.ytimg.com/vi/#{ youtube_id }/mqdefault.jpg">}
+    youtube_id
   end
 end
