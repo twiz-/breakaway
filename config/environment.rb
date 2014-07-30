33a61a7e172cf18cb2027faafec95fc38c77ce70
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-07-30 11:02:43 -0700

    remove from env and place into prod

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Breakaway::Application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'http://murmuring-sands-2665.herokuapp.com/' }
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Breakaway::Application.configure do
   config.action_controller.perform_caching = true
   
   # send email using postmark
+  config.action_mailer.default_url_options = { :host => 'http://murmuring-sands-2665.herokuapp.com/' }
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { api_key: ENV['PostMarkKeyID'] }
 
