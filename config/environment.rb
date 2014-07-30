011b6deedaf2a825eae93af2fbb536b22eda0162
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-07-30 11:01:01 -0700

    call config after initialization

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.default_url_options = { :host => 'http://murmuring-sands-2665.herokuapp.com/' }
-
 # Initialize the Rails application.
 Breakaway::Application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'http://murmuring-sands-2665.herokuapp.com/' }
