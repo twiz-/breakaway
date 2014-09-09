fa0720a0fd65e36f231d4ae2104501c472709cdf
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-09-09 13:39:57 -0700

    add youtube initial regex for user posting

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,9 @@
 class Listing < ActiveRecord::Base
   validates :video, :description, presence: :true
+  validates :video, format: {
+         with: /\Ahttps:\/\/www.youtube.com\//,
+         message: "Must be a youtube video url."
+       }
   
   belongs_to :club_player
 end
