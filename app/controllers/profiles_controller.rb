c5f0953fe4bc86e1d54c5a9b2286ec1492229fe9
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-09-10 14:53:04 -0700

    add simple validation for sending a msg

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -40,16 +40,24 @@ class ProfilesController < ApplicationController
   def email_club_coach        
     club_player = ClubPlayer.find_by_profile_name(params[:profile_name]).full_name    
     message = params[:message]    
-    notifier = Notifier.contact_club_coach(current_college_coach,message,club_player)    
-    if notifier.deliver
-      redirect_to profile_path(params[:profile_name]), notice: "Good your email has been sent and you'll receive a copy"
+    @notifier = Notifier.contact_club_coach(current_college_coach,message,club_player)        
+    if message.length >= 5
+      send_email
     else
-      redirect_to profile_path(params[:profile_name]), notice: "The msg couldn't be deliverd please try again later. If it happens again contact support"
-    end    
+      redirect_to profile_path(params[:profile_name]), notice: "The msg couldn't be delivered, was it longer than 5 characters? Please try again later. If it happens again contact support."  
+    end
   end    
   
   private 
+  
   def find_player
     @player = ClubPlayer.find_by_profile_name(params[:profile_name])
   end
+  
+  def send_email
+    if @notifier.deliver 
+      redirect_to profile_path(params[:profile_name]), notice: "Good your email has been sent and you'll receive a copy"      
+    end    
+  end
+  
 end
