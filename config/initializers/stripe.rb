3d1a11d8a298b049c3d0683f2b15f708383e0466
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-09-26 11:25:10 -0700

    Swap for live transactions

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['TEST_STRIPE_KEY_P'],
-  :secret_key      => ENV['TEST_STRIPE_KEY_S']
+  :publishable_key => ENV['LIVE_STRIPE_PUB_KEY'],
+  :secret_key      => ENV['LIVE_STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
