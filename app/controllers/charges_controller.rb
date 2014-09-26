b705f42d02b9c63e254116c0838d20ae7909c31e
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-09-26 12:06:49 -0700

    update correct plan in charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -22,7 +22,7 @@ class ChargesController < ApplicationController
     subscription.email                    = current_club_player.email
     subscription.stripe_customer_id       = customer.id
     subscription.created_at               = Date.today
-    subscription.stripe_subscription_name = ENV["LIVE_STRIPE_PLAN"]
+    subscription.stripe_subscription_name = ENV["LIVE_ANNUAL_PLAN"]
     subscription.save
     
     rescue Stripe::CardError => e
