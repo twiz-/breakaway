f7b903b2252b42af15cb19dae0a5d0232c14ca02
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-09-25 18:51:34 -0700

    trash excess verified

--- db/migrate/20140907044319_add_column_to_coaches.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToCoaches < ActiveRecord::Migration
-  def change
-    add_column :college_coaches, :verified, :boolean, default: false
-  end
-end
--- db/migrate/20140911005325_add_verified_to_college_coaches.rb
+++ db/migrate/20140911005325_add_verified_to_college_coaches.rb
@@ -1,5 +1,5 @@
 class AddVerifiedToCollegeCoaches < ActiveRecord::Migration
   def change
-    add_column :college_coaches, :verified, :boolean
+    add_column :college_coaches, :verified, :boolean, default: false
   end
 end
