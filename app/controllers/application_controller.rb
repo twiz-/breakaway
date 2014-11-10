c5fc645a0445a1232adca96e0ea1809c910a844c
Tony <tony.michaelramirez@gmail.com>
2014-11-10 09:19:29 -0800

    take away redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :check_domain
+  # before_filter :check_domain
   protect_from_forgery with: :exception
   \
   
